b5330c72e02a05e7bc0414b89d1e8f73a0f7d866
gingerpauls <hutchinson9736@gmail.com>
2023-02-12 18:48:07 -0800

    can now read from current inputfile.txt if exists. if not, create new text file and populate

--- CSLabs/CSLabs/CSLabs.cpp
+++ CSLabs/CSLabs/CSLabs.cpp
@@ -14,7 +14,27 @@ int main()
 	std::ofstream outToTextFile;
 
 	inFromTextFile.open("inputfile.txt");
+	if (!inFromTextFile)
+	{
+		printf("Can't open inputfile.txt successfully. I will create it for you!\n");
+
+		outToTextFile.open("inputfile.txt");
+
+		outToTextFile << "firstLine: paul" << std::endl;
+		outToTextFile << "secondLine: hutchinson" << std::endl;
+
+		outToTextFile.close();
+		inFromTextFile.open("inputfile.txt");
+		//return 1;
+	}
+
 	outToTextFile.open("outputfile.txt");
+	if (!outToTextFile)
+	{
+		printf("Can't open outputfile.txt successfully. I will create it for you!\n");
+		//outToTextFile.open("outputfile.txt");
+		//return 1;
+	}
 
 	printf("input line 1: ");
 	std::cin.getline(firstLine, sizeof firstLine);
@@ -26,9 +46,8 @@ int main()
 	outToTextFile << "secondLine: " << secondLine << std::endl;
 	inFromTextFile.getline(secondLine, sizeof secondLine);
 
-	printf("\nText file: \n%s \n%s\n\n", firstLine, secondLine);
+	printf("\nread from inputfile.txt: \n%s \n%s\n\n", firstLine, secondLine);
 
 	inFromTextFile.close();
 	outToTextFile.close();
-
 }
