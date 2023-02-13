ccb64642c92e69fd5ca9a78c4aaa2b422b62229b
gingerpauls <hutchinson9736@gmail.com>
2023-02-12 17:28:57 -0800

    getting and printing character arrays using getline() and printf()

--- CSLabs/CSLabs/CSLabs.cpp
+++ CSLabs/CSLabs/CSLabs.cpp
@@ -2,21 +2,20 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
 int main()
 {
-    int x = 5, y = 6;
-    std::cout << "new adding feature!\n";
-    std::cout << x << "+" << y << "=" << x+y;
+	const int characterArraySize = 80;
+	char firstName[characterArraySize];
+	char lastName[characterArraySize];
+
+	printf("First name: ");
+	std::cin.getline(firstName, sizeof firstName);
+
+	printf("\nLast name: ");
+	std::cin.getline(lastName, sizeof lastName);
+
+	printf("\nYour name is: %s %s\n\n", firstName, lastName);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
