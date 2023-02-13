77a5c5e5aacec48292f3a797a0fbac8efd5e0d47
gingerpauls <hutchinson9736@gmail.com>
2023-02-12 17:40:37 -0800

    read and write to text files using fstream

--- CSLabs/CSLabs/CSLabs.cpp
+++ CSLabs/CSLabs/CSLabs.cpp
@@ -1,5 +1,4 @@
 // CSLabs.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <string>
@@ -11,11 +10,27 @@ int main()
 	char firstName[characterArraySize];
 	char lastName[characterArraySize];
 
+	std::ifstream inFromTextFile;
+	std::ofstream outToTextFile;
+
+	inFromTextFile.open("inputfile.txt");
+	outToTextFile.open("outputfile.txt");
+
+
+
 	printf("First name: ");
 	std::cin.getline(firstName, sizeof firstName);
+	outToTextFile << firstName; //how to add newline to character array? or other way to do it?
+	inFromTextFile >> firstName;
 
 	printf("\nLast name: ");
 	std::cin.getline(lastName, sizeof lastName);
+	outToTextFile << lastName;
+	inFromTextFile >> lastName;
 
 	printf("\nYour name is: %s %s\n\n", firstName, lastName);
+
+	inFromTextFile.close();
+	outToTextFile.close();
+
 }
